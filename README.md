ca29b1bce91902e236f1714b8b1ffe11aeb3b6d9
[afaureve] <[tom.favereautpe@gmail.com]>
2025-06-13 15:08:53 +0300

    build

---
+++ README.md
@@ -0,0 +1,5 @@
+# phonebook
+
+exercice done for full stack open course of the University of Helsinki
+
+the application is host on : https://phonebook-v333.onrender.com/
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors())
 
 
